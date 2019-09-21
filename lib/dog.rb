79bc5366c34fed85a1483d97c08227df0c7725b8
Jonah Brown <jswish02@gmail.com>
2019-09-21 05:02:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+   @@all = []
+  def initialize(name)
+    @name = name 
+  
+  @@all << self
+  @@print_all
+end
+  def print_all
+    .print_all(Dog.name.all)
+end
+  def  it_self
+    self.all 
+  end
+  def clear_all
+  @@all.clear
+end
+end
